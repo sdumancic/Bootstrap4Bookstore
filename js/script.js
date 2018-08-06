b616e27710b7353422cc867c0ff8ecd5242ea93a
sdumancic <sanjin.dumancic@yahoo.com>
2018-08-06 10:45:12 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	$('.hamburger-menu').click(function(){
+		$('.navigation').toggleClass('change');
+	});
+
+	lightbox.option({
+		'resizeDuration':400,
+		'wrapAround':true
+	});
+
+	$(window).scroll(function(){
+		let position = $(this).scrollTop();
+		if (position >= 350){
+			$('.gallery').addClass('change');
+		} else{
+			$('.gallery').removeClass('change');
+		}
+	});
+
+	$('.writers-accordion').click(function(event){
+		if (event.target.id.split('-')[0] === 'button'){
+			// user has clicked the button
+			$('#book-1').attr('src','img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+			$('#book-2').attr('src','img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+		}
+		
+	});
+});
